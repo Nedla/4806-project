76462e50465373af3d185d43529fa1be4d5e1237
Conor <ccmckeen@gmail.com>
2019-03-03 20:27:20 -0500

    Added Maven, Spring, and test frameworks.                                                  Added config files for Travis CI and Heroku.                                                  Added 'hello world' basic application with tests.

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java $JAVA_OPTS -Dserver.port=$POST -jar target/*.jar -
+Dspring.profiles.active=prod
---
+++ src/test/java/project/test/SmokeTest.java
@@ -0,0 +1,22 @@
+package project.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import project.HomeController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private HomeController homeController;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(homeController).isNotNull();
+    }
+}
