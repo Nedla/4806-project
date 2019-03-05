9ad41fba37fcf57fced90ab8259826583feade44
Conor <ccmckeen@gmail.com>
2019-03-04 19:19:15 -0500

    Serving basic pages

--- src/main/java/project/HomeController.java
+++ src/main/java/project/HomeController.java
@@ -1,14 +1,31 @@
 package project;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public @ResponseBody String greeting() {
-        return "Hello World";
+    /*
+    @GetMapping("/")
+    public String greeting() {
+        return "index";
     }
+    */
+
+    @GetMapping("/author")
+    public String getAuthorPage() {
+        return "author";
+    }
+
+    @GetMapping("/editor")
+    public String getEditorPage() {
+        return "editor";
+    }
+
+    @GetMapping("/reviewer")
+    public String getReviewerPage() {
+        return "reviewer";
+    }
+
 }
