986c2aa17f4774963dee919facd947f740b3d655
Alden <aldenwang1997@gmail.com>
2019-03-18 00:13:00 -0400

    wip

--- src/main/java/project/ArticleController.java
+++ src/main/java/project/ArticleController.java
@@ -1,5 +1,7 @@
 package project;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,15 +9,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import project.storage.StorageService;
+import project.storage.StorageFileNotFoundException;
+
 @Controller
 public class ArticleController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public ArticleController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
     @Autowired
     ArticleRepository articleRepository;
 
     @GetMapping("/articleSubmitForm")
-    public String articleSubmitFormGet(Model model) {
+    public String articleSubmitFormGet(Model model) throws IOException {
         model.addAttribute("article", new Article());
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(ArticleController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
         return "articleSubmitForm";
     }
     @PostMapping("articleSubmitForm")
