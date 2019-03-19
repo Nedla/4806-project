371f37d2668c0ba49498bd264236c6958badf9ec
Nedla <aldenwang1997@gmail.com>
2019-03-19 15:54:17 -0400

    Multipart file upload added

--- src/main/java/project/ArticleController.java
+++ src/main/java/project/ArticleController.java
@@ -1,27 +1,72 @@
 package project;
 
+import java.io.IOException;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import project.storage.StorageService;
 
 @Controller
 public class ArticleController {
 
+    private final StorageService storageService;
+
     @Autowired
     ArticleRepository articleRepository;
 
+    @Autowired
+    public ArticleController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
     @GetMapping("/articleSubmitForm")
-    public String articleSubmitFormGet(Model model) {
+    public String articleSubmitFormGet(Model model) throws IOException {
         model.addAttribute("article", new Article());
+
+        model.addAttribute("files", storageService.loadAll().map(
+            path -> MvcUriComponentsBuilder.fromMethodName(
+                ArticleController.class, "serveFile", path.getFileName().toString())
+                .build().toString())
+                .collect(Collectors.toList()));
+
         return "articleSubmitForm";
     }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
     @PostMapping("articleSubmitForm")
-    public String articleSubmitFormPost(Article article) {
+    public String articleSubmitFormPost(@RequestParam("file") MultipartFile file, 
+    RedirectAttributes redirectAttributes, Article article) {
         article.setStatus("Submitted");
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename());
+
         articleRepository.save(article);
+
         return "redirect:/";
     }
+    
 }
