edc24ba3d2128c8c01d500eaf1cd16263383945b
Conor <ccmckeen@gmail.com>
2019-03-20 09:55:37 -0400

    UserController tests

--- src/test/java/project/UserControllerTest.java
+++ src/test/java/project/UserControllerTest.java
@@ -1,5 +1,6 @@
 package project;
 
+import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,11 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Assert;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.View;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -34,12 +38,45 @@ public class UserControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private UserController userController;
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
-    public void testLoginPage() throws Exception {
+    public void testLoginPageGet() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/loginPage"))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("user", userController.user))
                 .andExpect(view().name("loginPage"));
     }
+
+    @Test
+    public void testLoginPagePost() throws Exception {
+        Gson gson = new Gson();
+        User user = new User("name123", "pass123", User.Role.SUBMITTER.toString());
+        List<User> users = userRepository.findByUsernameAndPassword("name123", "pass123");
+        Assert.isTrue(users.isEmpty(), "User does not exist yet");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/loginPage")
+                .param("username", user.getUsername())
+                .param("password", user.getPassword())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(302))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(view().name("redirect:/"));
+
+        //TODO Repo check for update
+        //users = userRepository.findByUsernameAndPassword("name123", "pass123");
+        //Assert.isTrue(users.size() == 1, "User should be registered now");
+    }
+
+    @Test
+    public void testRegisterUserFormGet() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/registerUserForm"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("user", userController.user))
+                .andExpect(model().attribute("roles", User.Role.values()))
+                .andExpect(view().name("registerUserForm"));
+    }
+
 }
